a3be187f2517f0cfcdd42500d3746ecbffef547a
Christopher Read <christopherread22@outlook.com>
2020-12-10 02:11:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,11 +7,12 @@ end
 
 def monopoly_with_second_tier
  base_hash = {
-   :railroads => {},
-   :pieces => {"4"},
-   :rent_in_dollars => {},
-   :name => {} 
-   
+   :railroads => {
+     :pieces => 4,
+     :rent_in_dollars => {},
+     :names => {}
+   },
+  
  }
 
 end
